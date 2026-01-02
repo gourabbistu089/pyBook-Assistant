6499f7c6295f07d8094ca065ffda95ebb0a63c5c
Gourab Bistu <gourabbistu2002@gmail.com>
2026-01-02 13:02:19 +0530

    minor update

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version = '0.0.0',
-    author = 'Priyanshu',
-    author_email = 'priyanshu210021@gmail.com',
+    author = 'Gourab',
+    author_email = 'gourabbistu@gmail.com',
     packages=find_packages(),
     install_requires = []
 )
